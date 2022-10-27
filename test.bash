affc838a020fe6be6e3dffc6f60f17bfe9dad3ba
Fukuda Taiki <s21C1101AP@s.chibakoudai.jp>
2022-10-27 12:52:21 +0900

    test command

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 #PDX-FileCopyrightText: 2022 Taiki Fukuda
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -12,6 +12,14 @@ res=0
 out=$(seq 5 | ./plus)
 [ "${out}" = 15.0 ] || ng ${LINENO}
 
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng ${LINENO}
+[ "${put}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1 ] || ng ${LINENO}
+[ "${put}" = "" ] || ng ${LINENO}
+
 [ "$res" = 0 ] && echo OK
-exit $res
+ exit $res
 

d350a8d43fb288197dd382894b54052209e162ac
Fukuda Taiki <s21C1101AP@s.chibakoudai.jp>
2022-10-27 12:35:39 +0900

    test command

--- test.bash
+++ test.bash
@@ -1,6 +1,17 @@
 #!/bin/bash
+#PDX-FileCopyrightText: 2022 Taiki Fukuda
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
 
 out=$(seq 5 | ./plus)
+[ "${out}" = 15.0 ] || ng ${LINENO}
 
-[ "${out}" = 14.0 ]
+[ "$res" = 0 ] && echo OK
+exit $res
 

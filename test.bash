5c90b0f7f5d4f1fe051717632a472ca73bc199e7
Fukuda Taiki <s21C1101AP@s.chibakoudai.jp>
2022-11-03 12:08:45 +0900

    add a test

--- test.bash
+++ test.bash
@@ -14,11 +14,11 @@ out=$(seq 5 | ./plus)
 
 out=$(echo あ | ./plus)
 [ "$?" = 1 ] || ng ${LINENO}
-[ "${put}" = "" ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 out=$(echo | ./plus)
 [ "$?" = 1 ] || ng ${LINENO}
-[ "${put}" = "" ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
  exit $res

c7e2ca76846af8985f0ff1f7fc3d11cd115b3a1b
Fukuda Taiki <s21C1101AP@s.chibakoudai.jp>
2022-11-26 16:51:36 +0900

    update

--- test.bash
+++ test.bash
@@ -10,7 +10,7 @@ ng () {
 res=0
 
 out=$(seq 5 | ./plus)
-[ "${out}" = 15.0 ] || ng ${LINENO}
+[ "${out}" = [15.0 -15.0 120.0  0.008333333333333333 ] || ng ${LINENO}
 
 out=$(echo あ | ./plus)
 [ "$?" = 1 ] || ng ${LINENO}
